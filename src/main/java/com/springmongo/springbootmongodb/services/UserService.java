f5eea2be0310e096102bdce1b6dbfff65f8f1df2
gparizotto <g.parizotto@unesp.br>
2023-08-15 14:14:49 -0300

    added post, put and delete features for users

--- src/main/java/com/springmongo/springbootmongodb/services/UserService.java
+++ src/main/java/com/springmongo/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springmongo.springbootmongodb.domain.User;
+import com.springmongo.springbootmongodb.dto.UserDTO;
 import com.springmongo.springbootmongodb.repository.UserRepository;
 import com.springmongo.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,29 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("User not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		
+		return repo.save(newObj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	public void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 }
